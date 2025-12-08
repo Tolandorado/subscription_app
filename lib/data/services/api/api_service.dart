7490f450f357adc00d99bf1eaf914a4b70cb76e3
Tolandorado <tonisagaz@gmail.com>
2025-12-08 20:17:23 +0300

    feat: Add api service & rename storage

---
+++ lib/data/services/api/api_service.dart
@@ -0,0 +1,12 @@
+abstract class BaseApi {
+  Future<void> getSubscriptionStatus();
+}
+
+class MockApiService implements BaseApi {
+  @override
+  Future<void> getSubscriptionStatus() async {
+    await Future.delayed(const Duration(seconds: 1));
+    // В реальном приложении здесь была бы логика получения статуса подписки
+    // и возврата объекта SubscriptionInfoBase
+  }
+}
