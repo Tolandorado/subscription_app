9b9f91489ae66795a8ae4ac0de172ba2ae77de18
Tolandorado <tonisagaz@gmail.com>
2025-12-09 03:53:32 +0300

    refactor(navigation): Migrate navigation calls to GoRouter API

--- lib/viewmodels/home_viewmodel.dart
+++ lib/viewmodels/home_viewmodel.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:paywall_app/data/repositories/subscription_repository.dart';
 import 'package:paywall_app/data/models/subscription_info_base.dart';
 
@@ -10,7 +12,8 @@ class HomeViewModel {
 
   HomeViewModel(this._subscriptionRepository);
 
-  Future<void> resetSubscription() async {
+  Future<void> resetSubscription(BuildContext context) async {
     await _subscriptionRepository.resetSubscription();
+    context.go('/onboarding');
   }
 }
